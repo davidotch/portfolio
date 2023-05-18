9eef1a63e182784b1672c09ca24af00d5dc1b166
David Cardon <da.cardon@outlook.fr>
2023-05-18 19:28:49 +0200

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/home/home';
+import Home from './components/home/home';
+import About from './components/about/about';
 import Skills from './components/skills/skills';
 import Project from './components/project/project';
 import Contact from './components/contact/contact';
-import ErrorPage from './pages/ErrorPage/errorPage';
+import ErrorPage from './components/ErrorPage/errorPage';
 import Header from './components/Header/header';
 import Footer from './components/footer/footer';
 import './sass/main.scss';
@@ -12,11 +13,12 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Home />
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
       <Routes> 
-        <Route path="/" element={<Home />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/project/:id" element={<Project />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
       <Footer />
