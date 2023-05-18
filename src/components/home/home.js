05ee6f4ad022f97f415e205ff89bcd807bc124d0
David Cardon <da.cardon@outlook.fr>
2023-05-18 23:02:57 +0200

    modify all files

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,24 +1,16 @@
-import Background from '../../assets/desktop.jpg';
+import React from "react";
 
-export default function home() {
+const home = () => {
     return (
         <main className='home'>
-
-            <img className='home_img'
-                src={Background}
-                alt='laptop and glasses'               
-            />
-
-            <div className='home_container'>
-                <p className='home_title shadow-title'>
-                    &lsaquo;<span className='home_title--green'>div</span> <span className='home_title--violet'>className</span><span className='home_title--red'>=</span>"<strong>Portfolio</strong>"&rsaquo;<br />
-                    &emsp;&lsaquo;<span className='home_title--green'>h1</span>&rsaquo;Développeur, intégrateur web&lsaquo;<span className='home_title--green'>h1</span>/&rsaquo;<br />
-                    &lsaquo;<span className='home_title--green'>div</span> /&rsaquo;
-                </p>  
+            <div className='home_container shadow'>
+                <p className="home_container--nth">Portfolio</p>
+                <p className="home_container--nth2">Développeur</p>
+                <p className="home_container--nth3">Intégrateur Web</p>
+                <button href='#project' className='home_button'>Mes réalisations</button>
             </div>
-
-            
-              
         </main>
     );
 } 
+
+export default home
