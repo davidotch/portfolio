c1fcb57ba310f92b0b8a1b9b2ebba28ee2d3a04d
David Cardon <da.cardon@outlook.fr>
2023-05-18 20:07:00 +0200

    fix link

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,35 +1,43 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+// import { Link } from "react-router-dom";
 
-export default function Header() {
+export default function Header() { 
+
+  //change color scrolling header
+  const [color, setColor] = useState(false)
+  const changeColor = () => {
+    if (window.scrollY >= 90) {
+      setColor(true)
+    } else {
+      setColor(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeColor)
+
   return (
-    <header className="header">
+    <header className={color ? 'header header_color' : 'header'}>
+
         <a href="/" className="header_logo">CARDON DAVID</a>
-      <div className="header_link">
-        <Link
-          to="/"
-          className="header_link_active" 
-        >
-          Accueil
-        </Link>
-        <Link
-          to="/skills"
-          className="header_link_active"
-        >
-          Skills
-        </Link>
-        <Link
-          to="/project"
-          className="header_link_active"
-        >
-          Portfolio
-        </Link>
-        <Link
-          to="/contact"
-          className="header_link_active"
-        >
-          Contact
-        </Link>
-      </div>
+
+      <ul className="header_link">
+        <li className="header_link_active">
+          <a href="/">Accueil</a>
+        </li>
+        <li className="header_link_active">
+          <a href="#about">A propos</a>
+        </li>
+        <li className="header_link_active">
+          <a href="#skills">Skills</a>
+        </li>
+        <li className="header_link_active">
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li className="header_link_active">
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      
     </header>
   );
 }
