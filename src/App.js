8851be35f1052120a092c680ed9a9a31c7b5e501
David Cardon <da.cardon@outlook.fr>
2023-05-18 20:05:55 +0200

    fix errorPage route

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from './components/about/about';
 import Skills from './components/skills/skills';
 import Project from './components/project/project';
 import Contact from './components/contact/contact';
-import ErrorPage from './components/ErrorPage/errorPage';
+import ErrorPage from './pages/ErrorPage/errorPage';
 import Header from './components/Header/header';
 import Footer from './components/footer/footer';
 import './sass/main.scss';
