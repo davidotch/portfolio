e1d60decf14e1cb1f8fe6bcbdad23777d566e1b3
David Cardon <da.cardon@outlook.fr>
2023-05-21 18:47:03 +0200

    add about.js & about.scss

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,33 +1,43 @@
 import React from "react";
+import Photo from "../../assets/photo.jpg";
 
 const About = () => {
     return (
-        <div className='about' id='about'>
-            <div className="about_container">
-                <h2 className='about_title'>A Propos</h2>
+        <section className="about">
+            <span id="about"></span>
+                <h2>A propos</h2>
 
-                <hr />
+            <div className="about_info">
+                <div>
+                    <h3>Mon parcours</h3>
+                    <p>Après plusieurs années dans la maintenance automobile, j’ai décidé d’entreprendre une reconversion professionnelle dans un domaine alliant créativité et informatique: le Développement Web. j’ai donc complété mon parcours par une formation diplômante chez <a href="https://openclassrooms.com/fr/paths/594-integrateur-web" target="_blank" rel="noreferrer" title="Site de l'école"><strong>OpenClassrooms</strong></a> dans le but de devenir Développeur Web. </p>
 
-                <p className='about_text'>Je m'appelle David, j'ai 42 ans et je suis Développeur, Intégrateur Web Junior.</p>
+                    <h3>Soft skills</h3>
+                    <ul>
+                        <li>Esprit d'équipe</li>
+                        <li>Autonomie</li>
+                        <li>Curiosité</li>
+                        <li>Esprit d'entraide</li>
+                        <li>Persévérant</li>
+                        <li>Ouverture d'esprit</li>
+                    </ul>
 
-                <hr />
+                    <a className="curriculum" href="../../../public/files/CV_Cardon_David.pdf" target="_blank" download="CV David CARDON"> Télécharger mon CV</a>
+                </div>
 
-                <p className="about_text">Après une formation chez OpenClassrooms, j'ai acquis de nombreuses compétences et connaissances grâce à des projets réalisés dans différents langages de programmation.</p>
+                <div>
+                    <img className="profil" src={Photo} alt="profil" />
+                    <h3>David Cardon</h3>
+                    <q> "Le succès vient de la curiosité, de la concentration, de la persévérance et de l'autocritique."<br /><strong>Albert Einstein</strong></q>
+                    <ul>
+                        <li>42 ans</li>
+                        <li>Châteauroux</li>
+                        <li>Hobbies: Cyclisme, Guitare, Jeux Vidéo</li>
+                    </ul>
+                </div>
+            </div>
 
-                <hr />
-
-                <p className="about_text">Conscient du chemin à parcourir pour monter en compétences, je suis plus que jamais motivé à apprendre pour m'améliorer. Tout en prenant en compte que notre métier est en perpétuelle évolution, je considère qu'il est important de chercher continuellement à se former en repoussant les limites de ses connaissances.
-                </p>
-
-                <hr />
-
-                <p className="about_text">Alors si vous aussi, vous partagez ce point de vue et que vous souhaitez en savoir plus, n'hésitez pas à me <a href="#contact" className="about_text--contact"><strong>contacter</strong></a>.</p>
-
-                
-
-                
-            </div>      
-        </div>
+        </section>
     )
 }
 
