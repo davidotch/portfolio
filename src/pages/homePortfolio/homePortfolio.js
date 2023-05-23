f5f66835a0a009af4610db5e23ed3e268a4ac7e0
David Cardon <da.cardon@outlook.fr>
2023-05-24 01:33:10 +0200

    import route

---
+++ src/pages/homePortfolio/homePortfolio.js
@@ -0,0 +1,15 @@
+import Home from '../../components/home/home';
+import About from '../../components/about/about';
+import Skills from '../../components/skills/skills';
+
+const homePortfolio = () => {
+    return (
+        <>
+            <Home />
+            <About />
+            <Skills />
+        </>
+    )
+}
+
+export default homePortfolio
