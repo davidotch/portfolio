59d9fe960f7b14ce6e47567d20b4d46cb8d8273a
David Cardon <da.cardon@outlook.fr>
2023-05-24 23:18:29 +0200

    add description props

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 
-const Card = ({id, title, cover}) => {
+const Card = ({id, title, cover, description}) => {
     return (
         <Link to={`/accomodation/${id}`}
         className="card_accomodation">
             <img src={cover} alt={title} />
             <h3>{title}</h3>
+            <p>{description}</p>
         </Link>
     );
 }
