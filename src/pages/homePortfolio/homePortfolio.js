a548162fa8d100d9a70318c5eb34557c78c2383e
David Cardon <da.cardon@outlook.fr>
2023-05-24 23:15:56 +0200

    fix background

--- src/pages/homePortfolio/homePortfolio.js
+++ src/pages/homePortfolio/homePortfolio.js
@@ -1,6 +1,7 @@
 import Home from '../../components/home/home';
 import About from '../../components/about/about';
 import Skills from '../../components/skills/skills';
+import Gallery from '../../components/gallery/gallery';
 
 const homePortfolio = () => {
     return (
@@ -8,6 +9,7 @@ const homePortfolio = () => {
             <Home />
             <About />
             <Skills />
+            <Gallery />
         </>
     )
 }
