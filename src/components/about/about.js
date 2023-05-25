8424d2d588c25c3086bfe5730c648d56526063fa
David Cardon <da.cardon@outlook.fr>
2023-05-25 23:54:25 +0200

    fix cv access

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -22,7 +22,7 @@ const About = () => {
                         <li>Ouverture d'esprit</li>
                     </ul>
 
-                    <a className="curriculum" href="/CV_Cardon_David.pdf" target="self" download="CV David CARDON"> Télécharger mon CV</a>
+                    <a className="curriculum" href="/images/CV_Cardon_David.pdf" target="self" download="CV David CARDON"> Télécharger mon CV</a>
                 </div>
 
                 <div>
