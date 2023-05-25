37467770add5909eeee90b347d91a4ab73744e77
David Cardon <da.cardon@outlook.fr>
2023-05-26 00:49:43 +0200

    change name

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,8 +5,8 @@ const home = () => {
         <main className='home' id="home">
             <div className='home_container shadow'>
                 <p className="home_container--nth">Portfolio</p>
-                <p className="home_container--nth2">Développeur junior</p>
-                <p className="home_container--nth3">Intégrateur Web</p>
+                <p className="home_container--nth2">Intégrateur web</p>
+                <p className="home_container--nth3">Dévellopeur junior</p>
                 <a className='home_button' href="#project">Mes réalisations</a>
             </div>
         </main>
