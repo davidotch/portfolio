99ca5e339e91869836d8d607a7a9a39764e1d360
David Cardon <da.cardon@outlook.fr>
2023-05-25 09:12:26 +0200

    create footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,33 @@
+import React from 'react'
+
 const Footer = () => {
     return (
-    <div className="footer">
-      <div className="footer_copyright">
-        <p>&#169; 2023 Copyright - Cardon David. Tous droits réservés</p>
-      </div>
-    </div>
-    );
-} 
+        <div className='footer'>
+            <div className='container'>
+                <ul>
+                    <li className='nav-item'>
+                        <a href='/'>Accueil</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#about'>A propos</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#skills'>Compétences</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#project'>Portfolio</a>
+                    </li>
+                    <li className='nav-item'>
+                      <a href='#contact'>Contact</a>
+                    </li>
+                </ul>
+                <div className='bottom'>
+                    <span className='line'></span>
+                    <p>2020 Execute, Inc. All rights reserved</p>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Footer
