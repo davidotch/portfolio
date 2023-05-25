0ec41e3f228c0c153c2b0ea14cabe9f0b3fe1c20
David Cardon <da.cardon@outlook.fr>
2023-05-25 11:22:15 +0200

    add react-icons

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { FaGithubSquare } from 'react-icons/fa'
+import { FaLinkedin } from 'react-icons/fa'
+import { FaEnvelopeSquare } from 'react-icons/fa'
 
 const Footer = () => {
     return (
-        <div className='footer'>
+        <div id='contact' className='footer'>
             <div className='footer_container'>
                 <ul>
                     <li className='nav-item'>
@@ -17,12 +20,19 @@ const Footer = () => {
                     <li className='nav-item'>
                         <a href='#project'>Portfolio</a>
                     </li>
-                    <li className='nav-item'>
-                      <a href='#contact'>Contact</a>
+                </ul>
+                <ul>
+                    <li>
+                        <a href='https://github.com/davidotch' target='_blank' rel='noreferrer'><FaGithubSquare size={40} /></a>
+                    </li>
+                    <li>
+                        <a href='https://www.linkedin.com/in/david-cardon-b293a8199/' target='_blank' rel='noreferrer'><FaLinkedin size={40} /></a>
+                    </li>
+                    <li>
+                        <a href='mailto:da.cardon@outlook.fr'><FaEnvelopeSquare size={40} /></a>
                     </li>
                 </ul>
                 <div className='footer_bottom'>
-                    <span className='footer_line'></span>
                     <p>2023 David Cardon. All rights reserved</p>
                 </div>
             </div>
