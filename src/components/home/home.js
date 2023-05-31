5895da62b70c77fd210d31095f4d330ca3065c7e
David Cardon <da.cardon@outlook.fr>
2023-05-31 22:19:00 +0200

    fix classname & import css file

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,13 +1,14 @@
 import React from "react";
+import '../../style/home.css';
 
 const home = () => {
     return (
         <main className='home' id="home">
-            <div className='home_container shadow'>
-                <p className="home_container--nth">Portfolio</p>
-                <p className="home_container--nth2">Intégrateur web</p>
-                <p className="home_container--nth3">Dévellopeur junior</p>
-                <a className='home_button' href="#project">Mes réalisations</a>
+            <div className='container shadow'>
+                <p>Portfolio</p>
+                <p>Intégrateur web</p>
+                <p>Dévellopeur junior</p>
+                <a className='button' href="#project">Mes réalisations</a>
             </div>
         </main>
     );
