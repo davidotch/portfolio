738b21fd087a2fb8ebb3e6ddb1dd7120b651207e
David Cardon <da.cardon@outlook.fr>
2023-06-01 16:44:00 +0200

    fix className

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../../style/about.css';
 import Photo from "../../assets/photo.jpg";
 
 const About = () => {
@@ -7,7 +8,7 @@ const About = () => {
             <span id="about"></span>
                 <h2>A propos</h2>
 
-            <div className="about_info">
+            <div className="info">
                 <div>
                     <h3>Mon parcours</h3>
                     <p>Après plusieurs années dans la maintenance automobile, j’ai décidé d’entreprendre une reconversion professionnelle dans un domaine alliant créativité et informatique: le Développement Web. j’ai donc complété mon parcours par une formation diplômante chez <a href="https://openclassrooms.com/fr/paths/594-integrateur-web" target="_blank" rel="noreferrer" title="Site de l'école"><strong>OpenClassrooms</strong></a> dans le but de devenir Développeur Web.<br />Conscient du chemin à parcourir pour monter en compétences, je suis plus que jamais motivé à apprendre pour m'améliorer.<br />Tout en prenant en compte que notre métier est en perpétuelle évolution, je considère qu'il est important de chercher continuellement à se former en repoussant les limites de ses connaissances. </p>
