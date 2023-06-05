4ae83e2044bf1a8e952cc588b4c30dfe2b48ae31
David Cardon <da.cardon@outlook.fr>
2023-06-05 18:45:51 +0200

    add tech props

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,13 +1,18 @@
-// import { Link } from 'react-router-dom';
+import '../../style/card.css';
 
-const Card = ({title, cover, description, link}) => {
+const Card = ({title, cover, link, description, tech}) => {
     return (
-        <a href={link} target="_blank" rel="noreferrer"
-        className="card_accomodation">
-            <img src={cover} alt={title} />
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </a>
+        <div className="card">
+            <img src={cover} alt="" />
+                <div className='overlay'>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                    <span className='line'></span>
+                    <p><span className='tag'>{tech}</span></p>
+                    <span className='line'></span>
+                    <p><a href={link} target="_blank" rel="noreferrer">Accéder au site</a></p>
+                </div>
+        </div>
     );
 }
 
