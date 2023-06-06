3ecc7d3a4f4dd922bd9e37d779d0426f00963e9e
David Cardon <da.cardon@outlook.fr>
2023-06-06 16:53:19 +0200

    fix color

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -5,10 +5,10 @@ import '../../style/home.css';
 const home = () => {
     return (
         <main className='home' id="home">
-            <div className='container shadow'>
-                <p>Portfolio</p>
-                <p>Intégrateur web</p>
-                <p>Développeur junior</p>
+            <div className='container'>
+                <p className="shadow">Cardon David</p>
+                <p className="shadow">Intégrateur web</p>
+                <p className="shadow">Développeur junior</p>
                 <Link className='button' to="project" spy={true} smooth={true} offset={-50} duration={500}>Mes réalisations</Link>
             </div>
         </main>
