ed4988d9c5936c2da544813ada0d2319f8d3292a
David Cardon <da.cardon@outlook.fr>
2023-06-06 14:42:10 +0200

    fix animation icon

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,13 +10,13 @@ const Footer = () => {
             <div className='container-footer'>
                 <ul>
                     <li>
-                        <a href='https://github.com/davidotch' target='_blank' rel='noreferrer'><FaGithubSquare size={45} color='#f8f8f8' /></a>
+                        <a href='https://github.com/davidotch' target='_blank' rel='noreferrer'><FaGithubSquare className='iconHover' size={45} color='#f8f8f8' /></a>
                     </li>
                     <li>
-                        <a href='https://www.linkedin.com/in/david-cardon-b293a8199/' target='_blank' rel='noreferrer'><FaLinkedin size={45} color='#f8f8f8' /></a>
+                        <a href='https://www.linkedin.com/in/david-cardon-b293a8199/' target='_blank' rel='noreferrer'><FaLinkedin className='iconHover' size={45} color='#f8f8f8' /></a>
                     </li>
                     <li>
-                        <a href='mailto:da.cardon@outlook.fr'><FaEnvelopeSquare size={45} color='#f8f8f8' /></a>
+                        <a href='mailto:da.cardon@outlook.fr'><FaEnvelopeSquare className='iconHover' size={45} color='#f8f8f8' /></a>
                     </li>
                 </ul>
                 <div className='copyright'>
