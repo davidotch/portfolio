67b89a57be61e19dd561c8186ba07a282134783e
David Cardon <da.cardon@outlook.fr>
2023-06-11 19:26:28 +0200

    fix indentation

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -9,8 +9,7 @@ const Card = ({ title, cover, link, description, tech }) => {
       initial={{ opacity: 0, y: 50 }}
       whileInView={{ opacity: 1, y: 0, transition: { duration: 1, delay: 0.6 } }}>
       <img src={cover} alt="" />
-      <div
-        className="overlay">
+      <div className="overlay">
         <h4>{title}</h4>
         <p>{description}</p>
         <p className="line">Technologies</p>
