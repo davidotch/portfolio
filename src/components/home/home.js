31d1949762e4ca17431218a7610cadfde7063080
David Cardon <da.cardon@outlook.fr>
2023-06-11 17:58:30 +0200

    add framer-motion to home

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-scroll';
+import { motion } from 'framer-motion';
 import '../../style/home.css';
 
 const home = () => {
   return (
     <main className="home" id="home">
-      <div className="container">
+      <motion.div
+        className="container"
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0, transition: { duration: 1, delay: 0.6 } }}>
         <p className="shadow">Cardon David</p>
         <p className="shadow">Intégrateur web</p>
         <p className="shadow">Développeur junior</p>
         <Link className="button" to="project" spy={true} smooth={true} offset={-50} duration={500}>
           Mes réalisations
         </Link>
-      </div>
+      </motion.div>
     </main>
   );
 };
