e90fc79bc6f71881348be79385d914960a4c2360
David Cardon <da.cardon@outlook.fr>
2023-06-11 14:58:51 +0200

    delete route pageNotFound

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import Home from './pages/homePortfolio/homePortfolio';
-import PageNotFound from './pages/pageNotFound/pageNotfound';
 import Header from './components/Header/header';
 import Footer from './components/footer/footer';
-import './sass/main.scss';
 
 const PageLayout = () => (
   <>
@@ -21,7 +19,6 @@ function App() {
         <Route element={<PageLayout />}>
           <Route path="/" element={<Home />} />
         </Route>
-        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
