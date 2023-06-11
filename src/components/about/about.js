e37ce0dac908ed3eeac6fd6f5ea313cbaeafba5c
David Cardon <da.cardon@outlook.fr>
2023-06-11 16:11:22 +0200

    fix const{}=data

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import '../../style/about.css';
 import Photo from '../../assets/photo.jpg';
+import data from '../../datas/data.json';
+
 
 const About = () => {
+
+  const {
+    profilList,
+    softSkills,
+    quote,
+    quoteAuthor,
+    careerDescription,
+    careerAdditionalInfo
+  } = data;
+
   return (
     <section className="about">
       <span id="about"></span>
@@ -12,40 +24,21 @@ const About = () => {
         <div>
           <h3>Mon parcours</h3>
           <p>
-            Après plusieurs années dans la maintenance automobile, j’ai décidé d’entreprendre une
-            reconversion professionnelle dans un domaine alliant créativité et informatique: le
-            Développement Web. j’ai donc complété mon parcours par une formation diplômante chez{' '}
-            <a
-              className="openclassroom"
-              href="https://openclassrooms.com/fr/paths/594-integrateur-web"
-              target="_blank"
-              rel="noreferrer"
-              title="Site de l'école">
-              <strong>OpenClassrooms</strong>
-            </a>{' '}
-            dans le but de devenir Développeur Web.
+            {careerDescription}
             <br />
-            Conscient du chemin à parcourir pour monter en compétences, je suis plus que jamais
-            motivé à apprendre pour m'améliorer.
-            <br />
-            Tout en prenant en compte que notre métier est en perpétuelle évolution, je considère
-            qu'il est important de chercher continuellement à se former en repoussant les limites de
-            ses connaissances.{' '}
+            {careerAdditionalInfo}
           </p>
 
           <h3>Soft skills</h3>
           <ul>
-            <li>Esprit d'équipe</li>
-            <li>Autonomie</li>
-            <li>Curiosité</li>
-            <li>Esprit d'entraide</li>
-            <li>Persévérant</li>
-            <li>Ouverture d'esprit</li>
+            {softSkills.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
           </ul>
 
           <a
             className="curriculum"
-            href="/images/CV_Cardon_David.pdf"
+            href={Photo}
             target="_blank"
             rel="noopener noreferrer"
             download="CV David CARDON">
@@ -56,17 +49,16 @@ const About = () => {
 
         <div>
           <img className="profil" src={Photo} alt="profil" />
-          <h3>David Cardon</h3>
+          <h3>Cardon David</h3>
           <q>
-            Le succès vient de la curiosité, de la concentration, de la persévérance et de
-            l'autocritique.
+            {quote}
             <br />
-            <strong>Albert Einstein</strong>
+            <strong>{quoteAuthor}</strong>
           </q>
           <ul>
-            <li>42 ans</li>
-            <li>Châteauroux</li>
-            <li>Hobbies: Cyclisme, Guitare, Jeux Vidéo</li>
+            {profilList.map((profil, index) => (
+              <li key={index}>{profil}</li>
+            ))}
           </ul>
         </div>
       </div>
