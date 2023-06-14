5fb651491e24d0130a099f3134ea79501834bbd6
David Cardon <da.cardon@outlook.fr>
2023-06-14 16:10:06 +0200

    add headerData list menu

---
+++ src/components/Header/headerData.js
@@ -0,0 +1,22 @@
+export const HeaderData = [
+    {
+        title: 'Acceuil',
+        path: 'home',
+        cName: 'nav-item',
+    },
+    {
+        title: 'A propos',
+        path: 'about',
+        cName: 'nav-item',
+    },
+    {
+        title: 'Compétences',
+        path: 'skills',
+        cName: 'nav-item',
+    },
+    {
+        title: 'Portfolio',
+        path: 'project',
+        cName: 'nav-item',
+    },
+]
